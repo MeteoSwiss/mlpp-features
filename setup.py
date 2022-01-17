cdaf4f16db763d765a06e3e1bf4d92ecbd1a79e9
ned <daniele.nerini@meteoswiss.ch>
2022-01-17 16:27:35 +0100

    Find packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # minimal setup.y until poetry supports PEP660
 # https://github.com/python-poetry/poetry/issues/34
-from setuptools import setup
+from setuptools import setup, find_packages
 
 requirements = [
     "zarr",
@@ -14,4 +14,5 @@ setup(
     name="mlpp-features",
     packages=["mlpp_features"],
     install_requires=requirements,
+    packages=find_packages(include=['mlpp_features']),
 )
