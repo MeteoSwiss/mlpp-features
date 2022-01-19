95d80c45721cfd3985e8c9113ef68cd295973414
frazane <zanetta.francesco@gmail.com>
2022-01-19 16:54:48 +0100

    rename station to station_id

--- mlpp_features/utils.py
+++ mlpp_features/utils.py
@@ -27,7 +27,7 @@ class PreprocDatasetAccessor:
 
     def __post_init__(self):
         if "latitude" in self.ds:
-            if "station" in self.ds:
+            if "station_id" in self.ds:
                 self.selector = ps.EuclideanNearestSparse(self.ds)
             else:
                 self.selector = ps.EuclideanNearestIrregular(self.ds)
