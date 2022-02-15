cb8a4fa3a68b73a9b7f01933fe3e98a5c88e071c
ned <daniele.nerini@meteoswiss.ch>
2022-02-15 17:31:42 +0100

    Filter final list of coordinates

--- mlpp_features/decorators.py
+++ mlpp_features/decorators.py
@@ -2,6 +2,9 @@ from functools import wraps
 import xarray as xr
 
 
+KEEP_STA_COORDS = ["longitude", "latitude", "elevation", "owner_id"]
+
+
 def asarray(func):
     """
     Make every feature function return a DataArray names like the function itself.
@@ -14,6 +17,10 @@ def asarray(func):
             out = out.to_array(name=func.__name__).squeeze("variable", drop=True)
         elif isinstance(out, xr.DataArray):
             out = out.rename(func.__name__)
+        # cleanup coordinates
+        out = out.drop_vars(
+            [c for c in out.coords if c not in list(out.dims) + KEEP_STA_COORDS]
+        )
         return out
 
     return inner
