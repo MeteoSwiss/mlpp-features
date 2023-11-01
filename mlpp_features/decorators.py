42c14c68a3f7c2cfc3ecf3b183d4dac154e0ee87
ned <daniele.nerini@meteoswiss.ch>
2023-11-01 10:05:52 +0100

    Disable caching by default

--- mlpp_features/decorators.py
+++ mlpp_features/decorators.py
@@ -1,18 +1,22 @@
-from functools import wraps
-from importlib.resources import path
-import xarray as xr
-import pickle
-from pathlib import Path
-from itertools import chain
+import logging
 import os
-import tempfile
+import pickle
+from functools import wraps
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import xarray as xr
+
+LOGGER = logging.getLogger(__name__)
 
 KEEP_STA_COORDS = ["longitude", "latitude", "elevation", "owner_id"]
 
 
-CACHEDIR_PREFIX = Path(os.environ.get("MLPP_CACHE", f"{os.getcwd()}/mlpp_cache/"))
-CACHEDIR_PREFIX.mkdir(parents=True, exist_ok=True)
-CACHEDIR = tempfile.TemporaryDirectory(prefix=f"{CACHEDIR_PREFIX.as_posix()}/")
+CACHEDIR_PREFIX = os.environ.get("MLPP_CACHE")
+if CACHEDIR_PREFIX:
+    LOGGER.info(f"Caching is enabled: {CACHEDIR_PREFIX}")
+    Path(CACHEDIR_PREFIX).mkdir(parents=True, exist_ok=True)
+    CACHEDIR = TemporaryDirectory(dir=CACHEDIR_PREFIX)
 
 
 def out_format(
@@ -56,19 +60,25 @@ def cache(fn):
     Cache results on disk as pickle objects the first time `fn` is called,
     then loads them back when needed again.
 
+    To enable the cache, define the MLPP_CACHE environment variable pointing at the
+    cache directory.
+
     Note: this works only if the arguments are always the same. This will usually
     be the case when extracting features, but beware of this caveat!
     """
 
     @wraps(fn)
     def inner(*args, **kwargs):
-
+        if CACHEDIR_PREFIX is None:
+            return fn(*args, **kwargs)
         cachedfile = Path(CACHEDIR.name) / f"{fn.__name__}.pkl"
         if not cachedfile.is_file():
             out = fn(*args, **kwargs).compute()
+            LOGGER.debug(f"to cache: {cachedfile}")
             with open(cachedfile, "wb") as f:
                 pickle.dump(out, f, protocol=-1)
         else:
+            LOGGER.debug(f"from cache: {cachedfile}")
             with open(cachedfile, "rb") as f:
                 out = pickle.load(f)
         return out.chunk("auto").persist()
