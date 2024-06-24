2c34f17deab41ac9147c7cb90971d65b41da3713
louisPoulain <louis.poulainauzeau@meteoswiss.ch>
2024-06-24 18:26:24 +0200

    Convert distance to km

--- mlpp_features/terrain.py
+++ mlpp_features/terrain.py
@@ -137,7 +137,7 @@ def distance_to_alpine_ridge(
     points = [(lat, lon) for lat, lon in zip(stations.latitude, stations.longitude)]
     points_proj = exp.reproject_points(points, "epsg:2056")
     line_proj = exp.reproject_points(alpine_crest_wgs84, "epsg:2056")
-    distances = exp.distances_points_to_line(points_proj, line_proj)
+    distances = exp.distances_points_to_line(points_proj, line_proj) / 1000 # convert to km
     return xr.Dataset(
         coords={
             "station": stations.index,
