3f3582b7a7a9be2f5d8eb1f7f3588102aecf98f3
Louis Poulain--Auzéau <47986600+louisPoulain@users.noreply.github.com>
2024-11-11 16:24:54 +0100

    Add clim for cloud cover

--- mlpp_features/climatology.py
+++ mlpp_features/climatology.py
@@ -1,1 +1,28 @@
+import logging
+from typing import Dict
 
+import xarray as xr
+import numpy as np
+
+from mlpp_features.decorators import cache, inputs, out_format
+from mlpp_features import calc
+
+LOGGER = logging.getLogger(__name__)
+
+# Set global options
+xr.set_options(keep_attrs=True)
+
+
+@out_format()
+def cloud_area_fraction(
+    data: Dict[str, xr.Dataset], stations, reftimes, leadtimes, **kwargs
+) -> xr.DataArray:
+    """
+    Climatology of cloud cover: the values were averaged at +-1h and +-10 days to account for seasonal variability 
+    """
+    return (
+        data["climatology"]
+        .mlpp.get("cloud_area_fraction")
+        .mlpp.unstack_time(reftimes, leadtimes)
+        .astype("float32")
+    )
