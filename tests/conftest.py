b926cf09cc286856e4410a38a536ee1428767c5d
Louis Poulain--Auzéau <47986600+louisPoulain@users.noreply.github.com>
2024-11-21 14:39:44 +0100

    revert update

--- tests/conftest.py
+++ tests/conftest.py
@@ -279,7 +279,7 @@ def clim_dataset():
     def _data():
 
         variables = [
-            "cloud_area_fraction_rollingmean_1h_10d",
+            "cloud_area_fraction",
         ]
 
         stations = _stations_dataframe()
